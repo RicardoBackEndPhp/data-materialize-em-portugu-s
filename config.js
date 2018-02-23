46358dfdd80644c254ceb527b9a6d7adb7ec2bec
Ricardo de Oliveira <ricardo.reksystem@gmail.com>
2018-02-23 00:31:24 -0300

    incluindo sigla dos dias da semana em português

--- config.js
+++ config.js
@@ -2,6 +2,7 @@ $('.datepicker').pickadate({
     monthsFull: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
     monthsShort: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
     weekdaysFull: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sabádo'],
+    weekdaysLetter: ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'],
     weekdaysShort: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'],
     today: 'Hoje',
     clear: 'Limpar',
